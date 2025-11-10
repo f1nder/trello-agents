3ebe80236aff11b4c8bce7c6ec66e4f91c35de96
Aleksandr M <netstroix@gmail.com>
2025-11-10 15:28:23 +0700

    fix path

--- src/powerup/config/constants.ts
+++ src/powerup/config/constants.ts
@@ -1,11 +1,11 @@
 export const APP_NAME = "Card Agents Live Roster";
 // Colorful brand icon (used in manifest and non-section UI)
-export const ICON_PATH = "./icons/card-agents.svg";
+export const ICON_PATH = "/icons/card-agents.svg";
 // Monochrome gray icon required by Trello for card-back sections
-export const CARD_BACK_ICON_PATH = "./icons/card-agents-gray.svg";
-export const CARD_BACK_IFRAME = "./card-back.html";
-export const SETTINGS_IFRAME = "./settings.html";
-export const POWERUP_CONNECTOR = "./powerup.html";
+export const CARD_BACK_ICON_PATH = "/icons/card-agents-gray.svg";
+export const CARD_BACK_IFRAME = "/card-back.html";
+export const SETTINGS_IFRAME = "/settings.html";
+export const POWERUP_CONNECTOR = "/powerup.html";
 
 export const STORAGE_KEYS = {
   clusterConfig: "clusterConfig",
